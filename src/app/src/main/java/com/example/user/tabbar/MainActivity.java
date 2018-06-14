78c5c688514379f8c02eec00a64e5461c730b367
jose chu <jchudb93@gmail.com>
2018-06-14 01:58:08 -0500

    view pager

--- src/app/src/main/java/com/example/user/tabbar/MainActivity.java
+++ src/app/src/main/java/com/example/user/tabbar/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,18 +22,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final  String tag = "MainActivity";
+    private static final  String TAG = "MainActivity";
 
     private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting");
+
+        mSectionsPageAdapter = new SectionsPageAdapter((getSupportFragmentManager()));
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
     }
     private void setupViewPager(ViewPager viewPager) {
-        
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB 1");
+        adapter.addFragment(new Tab2Fragment(), "TAB 2");
+        adapter.addFragment(new Tab3Fragment(), "TAB 3");
+        viewPager.setAdapter(adapter);
     }
 
 }
